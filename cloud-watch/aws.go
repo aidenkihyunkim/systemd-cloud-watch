421888b8e7f67255c9cf01ad289d590209272f8c
Kihyun Kim <kikim@technoblood.com>
2018-03-16 15:05:18 +0900

    Fix compile errors

--- cloud-watch/aws.go
+++ cloud-watch/aws.go
@@ -8,7 +8,7 @@ import (
 	awsSession "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"os"
-	"strings"
+	//"strings"
 	lg "github.com/advantageous/go-logback/logging"
 )
 
